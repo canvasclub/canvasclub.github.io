a22b002aea3e832aebb34f79d8e12346ad7c02d2
Carlos Solares <csolares23@gmail.com>
2014-12-15 09:56:56 -0800

    updated front view so that the youtube vid is not fullscreen

--- app/controllers/front.js
+++ app/controllers/front.js
@@ -4,7 +4,6 @@ export default Ember.Controller.extend({
   actions: {
     downloadPressed: function() {
       console.log("downloadPressed");
-      // new GumroadOverlay("https://gum.co/KdhT?wanted=true&locale=true", 'product');
     }
   }
 });
