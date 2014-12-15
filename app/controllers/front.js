93a07689cc64d24d0d369e8a02666a21f5654c10
Carlos Solares <csolares23@gmail.com>
2014-12-15 14:27:47 -0800

    updated menu

--- app/controllers/front.js
+++ app/controllers/front.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    clickedSounds: function() {
+      console.log("clickedSounds");
+      // this.
+
+    },
+    isActived: function(){
+
+    },
     downloadPressed: function() {
       console.log("downloadPressed");
     }
