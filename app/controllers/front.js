c6c66d43b3850394e6a3f994d34c8b27eb57dc3d
Carlos Solares <csolares23@gmail.com>
2014-12-15 12:50:49 -0800

    hamburger menu experiment

--- app/controllers/front.js
+++ app/controllers/front.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    menuDrop: function() {
+      console.log("menuDrop");
+
+    },
     downloadPressed: function() {
       console.log("downloadPressed");
     }
