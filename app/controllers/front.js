410edb199afea10a0a58ff690a64e24ec5611652
Carlos Solares <csolares23@gmail.com>
2014-12-15 16:18:06 -0800

    fixed menu

--- app/controllers/front.js
+++ app/controllers/front.js
@@ -3,15 +3,23 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     clickedSounds: function() {
-      console.log("clickedSounds");
-      // this.
+      // console.log("clickedSounds");
+
+      if (this.isActived === true){
+        this.set("isActived", false)
+        console.log(this.get("isActived"))
+      }else{
+        this.set("isActived", true)
+        console.log(this.get("isActived"))
+      }
 
     },
-    isActived: function(){
-
-    },
-    downloadPressed: function() {
-      console.log("downloadPressed");
+    isActived: true,
+    clickedAway: function(){
+      console.log("clickedAway");
     }
+    // downloadPressed: function() {
+    //   console.log("downloadPressed");
+    // }
   }
 });
