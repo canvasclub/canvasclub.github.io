959228cf29fc6a657d4e30ccdffcffce66094f8d
Carlos Solares <csolares23@gmail.com>
2014-12-30 17:03:51 -0500

    now there is an iFrame popup

--- app/controllers/front.js
+++ app/controllers/front.js
@@ -6,17 +6,24 @@ export default Ember.Controller.extend({
       // console.log("clickedSounds");
 
       if (this.isActived === true){
-        this.set("isActived", false)
-        console.log(this.get("isActived"))
+        this.set("isActived", false);
+        console.log(this.get("isActived"));
       }else{
-        this.set("isActived", true)
-        console.log(this.get("isActived"))
+        this.set("isActived", true);
+        console.log(this.get("isActived"));
       }
 
     },
     isActived: true,
+    isDownloadSong: false,
     clickedAway: function(){
       console.log("clickedAway");
+    },
+    clickedDownloadSong: function(){
+      this.set("isDownloadSong", true);
+    },
+    exitDownloadSong: function(){
+      this.set("isDownloadSong", false);
     }
     // downloadPressed: function() {
     //   console.log("downloadPressed");
